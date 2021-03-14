c6ae505d34e461d6f290467b536e1c4518fff932
„michellesaric“ <msaric03@fesb.hr>
2021-03-14 23:40:02 +0100

    Checkbox now works, some minor css issues still present

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function ToDo({toDo}) {
+
+    const handleCheckIfDone = (e) => {
+        const title = document.querySelector(".to-do-list-member-title");
+        if(e.target.checked === true) {
+            title.style.color = "gray";
+            title.style.textDecoration = "line-through";
+        } else {
+            title.style.color = "#1a191a";
+            title.style.textDecoration = "none";
+        }
+    }
     return (
         <div className = "to-do-list-member">
             <div className = "to-do-list-member-container">
                 <h3 className = "to-do-list-member-title">{toDo.title}</h3>
                 <div className = "to-do-list-member-check">
                     <p className = "to-do-list-member-priority">{toDo.priority}</p>
-                    <input type = "checkbox" className = "check" />
+                    <input type = "checkbox" className = "check" onClick={handleCheckIfDone}/>
                 </div>
             </div>
             <hr className = "horizontal-line"/>
